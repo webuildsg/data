1fdbd7f5da62c6375590c2941aff70c7e9b8fd57
Sayanee <sayanee@gmail.com>
2015-11-08 15:31:14 +0800

    feat: (wip) get current day data. #2 #3

--- tasks/cleanup.js
+++ tasks/cleanup.js
@@ -5,24 +5,47 @@
 
 var fs = require('fs');
 var path = require('path');
+var moment = require('moment-timezone');
 
-function listFiles(type) {
+function listFilePaths(type) {
   var allFiles = fs.readdirSync('./' + type + '/v1');
-  var jsonFiles = [];
+  var jsonFilePaths = [];
 
   allFiles.forEach(function(file) {
     if (path.extname(file) === '.json') {
-      jsonFiles.push(file);
+      jsonFilePaths.push('../' + type + '/v1/' + file);
     }
   })
 
-  return jsonFiles;
+  return jsonFilePaths;
+}
+
+function getCurrentDayData(data, type) {
+  var today = moment(data.meta.generated_at);
+  var compare = type === 'events' ? 0 : 1;
+  var compareTime = type === 'events' ? 'start_time' : 'pushed_at';
+  var answer = {};
+  answer.meta = data.meta;
+  answer[ type ] = [];
+
+  data[ type ].forEach(function(element) {
+    if (today.diff(moment(element[ compareTime ]), 'days') === compare) {
+      answer[ type ].push(element);
+    }
+  })
+
+  answer.meta['total_' + type] = answer[ type ].length;
+
+  return answer;
 }
 
 // TODO: cleanup data
 function cleanup(type) {
-  console.log(listFiles(type));
+  listFilePaths(type).forEach(function(file, index) {
+    getCurrentDayData(require(file), type);
+  })
 }
 
-exports.listFiles = listFiles;
+exports.listFilePaths = listFilePaths;
+exports.getCurrentDayData = getCurrentDayData;
 exports.cleanup = cleanup;
--- test/cleanupSpec.js
+++ test/cleanupSpec.js
@@ -6,19 +6,47 @@ var cleanupLib = require('../tasks/cleanup');
 
 describe('Cleanup', function() {
   describe('when the data is events', function() {
-    describe('#listFiles', function() {
+    describe('#listFilePaths', function() {
       it('returns a list of all .json files', function() {
-        expect(cleanupLib.listFiles('events')[0]).to.contain('.json');
-        expect(cleanupLib.listFiles('events')).to.be.instanceof(Array);
+        expect(cleanupLib.listFilePaths('events')[0]).to.contain('.json');
+        expect(cleanupLib.listFilePaths('events')).to.be.instanceof(Array);
+      })
+    })
+
+    describe('#getCurrentDayData', function() {
+      it('returns events for only the upcoming day', function() {
+        var allEvents = require('./factory/allEvents');
+        var allEventsNum = allEvents.meta.total_events;
+        var todayEvents = cleanupLib.getCurrentDayData(allEvents, 'events');
+        var todayEventsNum = todayEvents.meta.total_events;
+
+        expect(todayEventsNum).to.be.below(allEventsNum);
+      })
+    })
+
+    describe('#cleanup', function() {
+      it('returns events for the upcoming day', function() {
+        cleanupLib.cleanup('events');
       })
     })
   })
 
   describe('when the data is repos', function() {
-    describe('#listFiles', function() {
+    describe('#listFilePaths', function() {
       it('returns a list of all .json files', function() {
-        expect(cleanupLib.listFiles('repos')[0]).to.contain('.json');
-        expect(cleanupLib.listFiles('repos')).to.be.instanceof(Array);
+        expect(cleanupLib.listFilePaths('repos')[0]).to.contain('.json');
+        expect(cleanupLib.listFilePaths('repos')).to.be.instanceof(Array);
+      })
+    })
+
+    describe('#getCurrentDayData', function() {
+      it('returns repos for only the previous day', function() {
+        var allRepos = require('./factory/allRepos');
+        var allReposNum = allRepos.meta.total_repos;
+        var todayRepos = cleanupLib.getCurrentDayData(allRepos, 'repos');
+        var todayReposNum = todayRepos.meta.total_repos;
+
+        expect(todayReposNum).to.be.below(allReposNum);
       })
     })
   })
