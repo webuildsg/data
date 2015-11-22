ae83499b1612897af6bda50076094edeb9433185
Sayanee <sayanee@gmail.com>
2015-11-22 20:29:31 +0800

    design (wip): responsive bar graph #13

--- public/js/bar-graph.js
+++ public/js/bar-graph.js
@@ -7,48 +7,27 @@ function drawGraph(type, data, attr) {
     max_n = Math.max(data[d].n, max_n);
   }
 
-  var svg = d3.select('#' + type)
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h);
+  var graph = d3
+    .select('#' + type)
+    .append('div');
 
-  var dx = w / max_n;
-  var dy = h / data.length;
-  var bars = svg.selectAll('.bar')
+  var bars = graph.selectAll('.bar')
     .data(data)
     .enter()
-    .append('rect')
+    .append('div')
     .attr('class', function(d, i) {
       return 'bar ' + d.language;
     })
-    .attr('x', function(d, i) {
-      return 0;
+    .style('width', function(d, i) {
+      return (d.n/max_n * 100) + '%'
     })
-    .attr('y', function(d, i) {
-      return dy * i;
-    })
-    .attr('width', function(d, i) {
-      return dx*d.n
-    })
-    .attr('height', dy);
-
-  var text = svg.selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
+    .append('p')
     .attr('class', function(d, i) {
       return attr + ' ' + d[ attr ];
-    })
-    .attr('x', 5)
-    .attr('y', function(d, i) {
-      return dy*i + 15;
     })
     .text( function(d) {
       return d[ attr ] + ' (' + d.n  + ')';
     })
-    .attr('font-size', '15px')
-    .attr('fill', 'green')
-    .style('font-weight', 'bold')
 }
 
 [
