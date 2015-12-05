b5536db9ee57d964ce7ed05a2cee02d4bb62023c
Sayanee <sayanee@gmail.com>
2015-12-05 13:49:15 +0800

    refactor: build graph from one main index.js file

--- graphs/eventsPerGroup.js
+++ graphs/eventsPerGroup.js
@@ -2,6 +2,43 @@
 
 var utilsLib = require('../tasks/utils');
 
+function getData(source) {
+  var attr = 'group_name';
+  var answer = [];
+  var groups = [];
+
+  source.forEach(function(file) {
+    var data = require('.' + file);
+
+    data.events.forEach(function(ev) {
+      var attribute = ev[ attr ];
+
+      if (groups.indexOf(attribute) < 0) {
+        if (attribute) {
+          groups.push(attribute)
+          answer.push({
+            group: attribute,
+            n: 1,
+            url: ev.group_url
+          })
+        }
+      } else {
+        answer.forEach(function(a) {
+          if (a.group === ev[ attr ]) {
+            a.n += 1;
+          }
+        })
+      }
+    })
+  })
+
+  answer = getMoreThan(answer, 9);
+  answer = removeLocationString(answer);
+  answer = utilsLib.sortByAlphabet(answer, 'group');
+
+  utilsLib.publishData('events-per-group', answer);
+}
+
 function getMoreThan(array, number) {
   var answer = [];
 
@@ -38,41 +75,4 @@ function removeLocationString(array) {
   return array;
 }
 
-function getData(attr) {
-  var type = 'events';
-  var answer = [];
-  var groups = [];
-
-  utilsLib.listFilePaths(type).forEach(function(file) {
-    var data = require('.' + file);
-
-    data.events.forEach(function(ev) {
-      var attribute = ev[ attr ];
-
-      if (groups.indexOf(attribute) < 0) {
-        if (attribute) {
-          groups.push(attribute)
-          answer.push({
-            group: attribute,
-            n: 1,
-            url: ev.group_url
-          })
-        }
-      } else {
-        answer.forEach(function(a) {
-          if (a.group === ev[ attr ]) {
-            a.n += 1;
-          }
-        })
-      }
-    })
-  })
-
-  answer = getMoreThan(answer, 9);
-  answer = removeLocationString(answer);
-  answer = utilsLib.sortByAlphabet(answer, 'group');
-
-  utilsLib.publishData('events-per-group', answer);
-}
-
-getData('group_name');
+exports.getData = getData;
--- graphs/index.js
+++ graphs/index.js
@@ -7,6 +7,8 @@ var eventSource = utilsLib.listFilePaths('events');
 var totalEventsByDuration = utilsLib.getTotalByProperty('events', 'duration');
 var eventsPerDayOfWeekLib = require('./eventsPerDayOfWeek');
 var eventsPerDurationLib = require('./eventsPerDuration');
+var eventsPerGroupLib = require('./eventsPerGroup');
 
 utilsLib.publishData('events-per-day-of-week', eventsPerDayOfWeekLib.getData(eventSource));
 utilsLib.publishData('events-per-duration', eventsPerDurationLib.getData(totalEventsByDuration));
+utilsLib.publishData('events-per-group', eventsPerGroupLib.getData(eventSource));
