4245957ddea15dddc52fcbf29564b858b99aa79d
Sayanee <sayanee@gmail.com>
2015-12-06 19:21:05 +0800

    feat: display overview number on homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,6 +1,17 @@
 (function () {
   'use strict';
 
+  function callRoute(options, callback) {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', options.url, true);
+    request.responseType = 'json';
+    request.onload = function() {
+      callback(request.response);
+    }
+    request.send();
+  }
+
   // search bar
   var searchStyle = document.getElementById('search_style');
   var searchBox = document.getElementById('search');
@@ -15,4 +26,16 @@
       searchStyle.innerHTML = '.searchable:not([data-index*=\"' + this.value.toLowerCase() + '\"]) { display: none; }';
     });
   }
+
+  // fill in overview numbers
+  callRoute({
+    url: '/public/data/overview.json'
+  }, function(list) {
+    var overviewNumbers = list[ 0 ];
+
+    document.getElementById('total-repos').innerHTML = overviewNumbers.total_uniq_repos_updated;
+    document.getElementById('total-updates').innerHTML = overviewNumbers.total_updates_for_repos;
+    document.getElementById('total-events').innerHTML = overviewNumbers.total_events_held;
+    document.getElementById('total-groups').innerHTML = overviewNumbers.total_user_groups_held_events;
+  })
 })();
