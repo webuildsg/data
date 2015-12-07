a2d7ce15edb41343f1cb73769205bf20dcfdf2f2
Sayanee <sayanee@gmail.com>
2015-12-07 09:42:48 +0800

    refactor: simpler gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var jadeOptions = {
+  pretty: true
+};
 
 gulp.task('default', function() {
-  gulp.src('./views/index.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./'))
-
-  gulp.src('./views/graphs/**/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dataset'))
+  gulp.src('./views/index.jade').pipe(jade(jadeOptions)).pipe(gulp.dest('./'))
+  gulp.src('./views/graphs/**/*.jade').pipe(jade(jadeOptions)).pipe(gulp.dest('./dataset'))
 });
