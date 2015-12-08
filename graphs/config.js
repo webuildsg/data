4d1ba89bec364309038d7929a5c0727a4f202dd7
Sayanee <sayanee@gmail.com>
2015-12-08 13:58:19 +0800

    fix: remove some string for addresses for proper lat/long results. #37

--- graphs/config.js
+++ graphs/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   addressesToFix: [
     'King George\'s Building',
-    'Marina Bay Financial Centre Tower 2,',
+    '#23-01, Marina Bay Financial Centre Tower 2, ',
+    'Level 14,',
+    'Level 21 Auditorium,',
     '#09-01 Suntec Tower Five, 038985 Nearest Mrt:Promenade(Yellow Line),',
     'TradeGecko Office,',
     'Lowercase,',
