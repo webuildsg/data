ae8ab14693a8428b7429577def5ae68d49f988a2
Sayanee <sayanee@gmail.com>
2016-02-26 10:38:10 +0800

    refactor: pass all tests and checks. close #49

--- graphs/eventsPerWeekOfMonth.js
+++ graphs/eventsPerWeekOfMonth.js
@@ -1,6 +1,5 @@
 'use strict'
 
-var utilsLib = require('../tasks/utils')
 var moment = require('moment-timezone')
 
 function getData (source) {
@@ -12,8 +11,6 @@ function getData (source) {
     { week: 4, n: 0 },
     { week: 5, n: 0 }
   ]
-  var currentWeek
-  var attr = 'generated_at'
 
   source.forEach(function (file) {
     var totalEvents = require('.' + file).meta.total_events
@@ -26,28 +23,28 @@ function getData (source) {
   })
 
   answer = setLabelsForGraph(answer)
-  
+
   return answer
 }
 
-function getWeekOfMonth(date) {
+function getWeekOfMonth (date) {
   var day = date.getDate()
   var prefixes = ['0', '1', '2', '3', '4', '5']
 
-  day -= (date.getDay() == 0 ? 6 : date.getDay() - 1)
-  //get monday of this week
-  //special case handling for 0 (sunday)
+  day -= (date.getDay() === 0 ? 6 : date.getDay() - 1)
+  // get monday of this week
+  // special case handling for 0 (sunday)
 
   day += 7
-  //for the first non full week the value was negative
+  // for the first non full week the value was negative
 
   return prefixes[0 | (day) / 7]
 }
 
-function setLabelsForGraph(data) {
+function setLabelsForGraph (data) {
   var reply = []
 
-  data.forEach(function(eachData) {
+  data.forEach(function (eachData) {
     var num = eachData.week
 
     reply.push({
