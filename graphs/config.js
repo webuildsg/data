0ab4a80d5df73b49c59067b6456ba3154f282775
Sayanee <sayanee@gmail.com>
2016-03-07 14:38:06 +0800

    fix: use correct string for geocoding PayPal. #50

--- graphs/config.js
+++ graphs/config.js
@@ -6,13 +6,16 @@ module.exports = {
     '#23-01, Marina Bay Financial Centre Tower 2, ',
     'Level 14,',
     'Level 21 Auditorium,',
-    '#09-01 Suntec Tower Five, 038985 Nearest Mrt:Promenade(Yellow Line),',
     'TradeGecko Office,',
     'Akamai Singapore office, 1 Raffles Place, #16-61,',
     'Lowercase,',
+
     'Hive@Sandcrawler, ',
     'Sandcrawler',
-    '#09-01 Suntec Tower Five, '
+
+    '#09-01 Suntec Tower Five, 038985 Nearest Mrt:Promenade(Yellow Line),',
+    '#09-01 Suntec Tower Five, ',
+    '#07-01 Suntec Tower Five, Nearest Mrt:Promenade(Yellow Line), '
   ],
   addressesToFixByAdding: [
     'ArtScience Museum 6 Bayfront Ave Singapore 018974',
