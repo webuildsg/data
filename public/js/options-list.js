a6a68cc1ce708b27b117e5cf25bc907e58daebe7
Sayanee <sayanee@gmail.com>
2018-08-22 18:47:12 +0800

    Fix URLs

--- public/js/options-list.js
+++ public/js/options-list.js
@@ -38,7 +38,7 @@
   }
 
   callRoute({
-    url: '/public/data/repos-per-programming-language.json'
+    url: './public/data/repos-per-programming-language.json'
   }, function (list) {
     reposPerProgrammingLanguageList = list
 
